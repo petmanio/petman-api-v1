85e197de9138e0deeab56df476faefce4a899549
andantonyan <andantonyan@gmail.com>
2017-03-25 02:59:57 +0400

    feat(blog): add count to list

--- api/services/BlogService.js
+++ api/services/BlogService.js
@@ -4,6 +4,18 @@ module.exports = {
   },
 
   getList(skip = 0, limit = 10) {
-    return Blog.find({}).skip(skip).limit(limit);
+    let blogCount;
+    return Blog.count()
+      .then(count => {
+        blogCount = count;
+        return Blog.find({}).skip(skip).limit(limit);
+      })
+      .then(list => {
+        return {
+          count: blogCount,
+          list
+        }
+      });
+
   }
 };
