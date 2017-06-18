6186bfb665fc53a1f20b59f0362c9a1da7c99fec
andantonyan <andantonyan@gmail.com>
2017-06-18 20:09:22 +0400

    feat(questions): routes

---
+++ api/controllers/QuestionController.js
@@ -0,0 +1,52 @@
+/**
+ * QuestionController
+ *
+ * @description :: Server-side logic for managing Shops
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+const config = sails.config;
+const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
+const Q = require('q');
+
+module.exports = {
+	list(req, res, next) {
+	  Question.getList(req.query.skip, req.query.limit)
+      .then(questions => res.ok(questions))
+      .catch(next);
+  },
+
+  create(req, res, next) {
+    Question.create({
+      description: req.body.description,
+      user: req.pmUser,
+      images: images.map(image => {
+        return { src: image.fd.replace(config.uploadDir, '') }
+      })
+    })
+    .then(question => res.ok(question.toJSON()))
+    .catch(next)
+  },
+
+  getById(req, res, next) {
+	  return Question.getQuestionById(req.pmQuestion.id)
+      .then(question => {
+        if (req.pmUser) {
+          question.isOwner = question.user.id === req.pmUser.id;
+        } else {
+          question.isOwner = false;
+        }
+        res.json(question)
+      })
+      .catch(next)
+  },
+
+  deleteById(req, res, next) {
+    Question.deleteById(req.pmQuestion.id)
+      .then(res.ok())
+      .catch(next)
+  },
+
+};
+
